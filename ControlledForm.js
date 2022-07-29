dca830c7f08280d1025ce513800792f22300c316
Jenny <jfperham@gmail.com>
2022-07-29 11:04:29 -0700

    added example of controlled form

---
+++ ControlledForm.js
@@ -0,0 +1,85 @@
+function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  function onSubmit() {
+    console.log("Name value: " + name);
+    console.log("Email value: " + email);
+  }
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
+import { useState } from "react";
+const RegistrationForm = (props) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    processSubmittedDataFunction(formData);
+    setFormData({
+      name: "",
+      email: "",
+      password: "",
+    });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+};
---
+++ ToDoLater.md
@@ -0,0 +1,3 @@
+# To worry about later:
+- Heroku deployment:
+  - To get us through the tutorial, we initially configured our cluster to accept connections from any IP address but it’s not recommended. Ideally it would be more secure to restrict access to only the Web Application and developer(s), and there are a few ways to do this on Heroku.
